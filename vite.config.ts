f6d3698ee74c41273690909f1ed4fbe7824d849d
Rodrigo Tavares <rodrigo.tavares@wetrack.pt>
2025-01-26 03:25:19 +0000

    Debugging railway ports #3

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: { exclude: ['lucide-react'] },
   server: { 
-    host: '0.0.0.0',
-    port: 8080
+    port: 8080,
+    host: '0.0.0.0'
   }
 });
